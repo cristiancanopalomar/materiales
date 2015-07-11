5c04c79e9b44598a5ea79c0d7e1c4033a8a6f3c2
CRISTIAN CANO PALOMAR <cristiancanopalomar@gmail.com>
2015-07-11 15:43:22 -0500

    add celery
    agregamos celery a nuestro proyecto, esto nos permite trabar con tareas
    asíncronas.

--- materiales/settings.py
+++ materiales/settings.py
@@ -81,3 +81,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# celery conf
+CELERY_ACCEPT_CONTENT = ['pickle', 'json', 'msgpack', 'yaml']
+CELERY_TIMEZONE = TIME_ZONE
