632f9a59cebd5b0885e660f10042f0b87150cc9d
CRISTIAN CANO PALOMAR <cristiancanopalomar@gmail.com>
2015-07-11 17:36:13 -0500

    add table Component

--- basics/models.py
+++ basics/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from .rename import rename_file
 
 class Process(models.Model):
     type_code = (
         ('M', 'material'),
         ('A', 'activity'),
     )
-    code = models.PositiveIntegerField()
+    code_process = models.PositiveIntegerField()
     description = models.CharField(
         max_length=15,
         help_text='description of the material or activity',
@@ -14,14 +15,60 @@ class Process(models.Model):
         max_length=1,
         choices=type_code,
     )
-    active = models.BooleanField(
+    active_process = models.BooleanField(
         default=True
     )
 
     class Meta:
-        unique_together = ('code', 'type_process')
+        unique_together = ('code_process', 'type_process')
         verbose_name = u'code [process - activity]'
         verbose_name_plural = u'code [process - activity]'
+
+    def __unicode__(self):
+        return unicode(self.description)
+
+
+class Component(models.Model):
+    type_unit = (
+        ( 'M', 'meters'),
+        ('CU', 'units' ),
+    )
+    type_process = models.ForeignKey(
+        'Process',
+        limit_choices_to={
+            'type_process': 'M',
+        },
+    )
+    code_component = models.PositiveIntegerField(
+        unique=True,
+    )
+    code_sap = models.CharField(
+        max_length=10,
+        primary_key=True,
+        unique=True,
+        help_text='material code (trading system SAP)',
+    )
+    description = models.CharField(
+        max_length=100,
+        help_text='description of the Component',
+    )
+    image = models.ImageField(
+        upload_to=rename_file(
+            'upload/basic/component',
+        ),
+    )
+    unit = models.CharField(
+        max_length=2,
+        choices=type_unit,
+    )
+    active_component = models.BooleanField(
+        default=True,
+    )
+
+    class Meta:
+        unique_together = ('code_component', 'code_sap', 'description')
+        verbose_name = u'component not serialized'
+        verbose_name_plural = u'component not serialized'
 
     def __unicode__(self):
         return unicode(self.description)
---
+++ basics/rename.py
@@ -0,0 +1,13 @@
+import os
+from uuid import uuid4
+from django.utils.deconstruct import deconstructible
+
+@deconstructible
+class rename_file(object):
+    def __init__(self, sub_path):
+        self.path = sub_path
+
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        filename = '{}.{}'.format(uuid4().hex, ext)
+        return os.path.join(self.path, filename)
