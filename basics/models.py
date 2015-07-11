aa49e92fb11230200b78ae4186780434a65f8374
CRISTIAN CANO PALOMAR <cristiancanopalomar@gmail.com>
2015-07-11 16:22:59 -0500

    add table Process

--- basics/models.py
+++ basics/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Process(models.Model):
+    type_code = (
+        ('M', 'material'),
+        ('A', 'activity'),
+    )
+    code = models.PositiveIntegerField()
+    description = models.CharField(
+        max_length=15,
+        help_text='description of the material or activity',
+    )
+    type_process = models.CharField(
+        max_length=1,
+        choices=type_code,
+    )
+    active = models.BooleanField()
+
+    class Meta:
+		unique_together = ('code', 'type_process')
+		verbose_name = u'code [process - activity]'
+		verbose_name_plural = u'code [process - activity]'
+
+	def __unicode__(self):
+		return unicode(self.description)
