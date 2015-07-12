15417807927cf06c72aa2e8d5e8d0aebfb24bafd
CRISTIAN CANO PALOMAR <cristiancanopalomar@gmail.com>
2015-07-12 17:44:02 -0500

    serving static

--- materials/urls.py
+++ materials/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
@@ -7,4 +8,10 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+)
+
+# serving static
+urlpatterns += patterns('',
+	url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,}),
+	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT,}),
 )
